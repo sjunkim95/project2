cfb60776ba18fdf781d7ed19b374eacd8b714671
sjunkim95 <sangjunkim23@gmail.com>
2024-08-12 03:20:44 -0700

    commit

--- DictTuple.py
+++ DictTuple.py
@@ -6,12 +6,11 @@ class DictTuple:
         self.dt = [dictionaries for dictionaries in kwargs]
         print("받은값: ", self.dt)
 
-        print(type(self.dt))
 
         if len(self.dt) == 0:
             raise AssertionError("Dictionary is empty")
-        print(len(self.dt))
-        for i in range(len(self.dt)):
+
+        if type(self.dt[0]) == dict:
             if len(self.dt[0]) == 0:
                 raise AssertionError("Dictionary is empty")
         else:
@@ -164,11 +163,11 @@ class DictTuple:
         return my_list
 
 
-#coordinate = mynamedtuple('coordinate', 'x y')
-#d = DictTuple({'c1': coordinate(1, 2)}, {'c1': coordinate(3, 4)})
-#d1 = DictTuple({'c1': coordinate(1, 2)}, {'c1': coordinate(3, 4)})
-#d2 = DictTuple({'c2': coordinate(1, 2)}, {'c3': coordinate(3, 4)})
-#d4 = DictTuple({'c1': coordinate(1, 2)}, {'c1': coordinate(3, 4)}, {'c2': coordinate(1, 2)}, {'c3': coordinate(3, 4)})
+coordinate = mynamedtuple('coordinate', 'x y')
+d = DictTuple({'c1': coordinate(1, 2)}, {'c1': coordinate(3, 4)})
+d1 = DictTuple({'c1': coordinate(1, 2)}, {'c1': coordinate(3, 4)})
+d2 = DictTuple({'c2': coordinate(1, 2)}, {'c3': coordinate(3, 4)})
+d4 = DictTuple({'c1': coordinate(1, 2)}, {'c1': coordinate(3, 4)}, {'c2': coordinate(1, 2)}, {'c3': coordinate(3, 4)})
 
 #print("d4['c1']", d4['c1'])
 #print("d4 :", d4)
@@ -191,8 +190,8 @@ class DictTuple:
 # Contains
 #print("contains: ", d4.__contains__('c2'))
 #GetItems
-#print("getitems: ", d4.__getitem__('c1'))
-#print("getitems 이후: ", d4('c1'))
+print("getitems: ", d4.__getitem__('c1'))
+print("getitems 이후: ", d4('c1'))
 # newGet
 #p4 = DictTuple({'a': 1, 'b': 2, 'c': 3}, {'c': 13, 'd': 14, 'e': 15}, {'e': 25, 'f': 26, 'g': 27})
 #print("getItems: ", p4.__getitem__('e'))
@@ -205,7 +204,7 @@ class DictTuple:
 
 # __add__
 # 1.
-#print("d1+d2 는: ", d2+d1)
+print("d1+d2 는: ", d2+d1)
 
 # 2.
 #adt = DictTuple({'c1': coordinate(1, 2)}, {'c1': coordinate(3, 4)})
@@ -222,7 +221,7 @@ class DictTuple:
 #p1 = DictTuple({'a': 1, 'b': 2}, {'b': 12, 'c': 13})
 #p2 = DictTuple({'a': 1, 'b': 12}, {'c': 13})
 #print("더하기", p+p2)
-#p3 = DictTuple({'a': 1, 'b': 2}, {}, {'a': 1, 'b': 2})
+#p3 = DictTuple()
 #print("없다면:", p3)
 #print("eq: False", p1 == p2)
 #print("eq: True", p == p1)
