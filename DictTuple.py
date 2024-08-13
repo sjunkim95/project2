552741aebede6f8003d3dc0bde78f6bd369e0bc6
sjunkim95 <sangjunkim23@gmail.com>
2024-08-12 20:57:21 -0700

    commit

--- DictTuple.py
+++ DictTuple.py
@@ -138,7 +138,7 @@ class DictTuple:
         print("여기", self.dt, left)
         if type(left) is dict:
             self.dt.insert(0, left)
-            return tuple(self.dt + left.dt)
+            return tuple(left.dt + self.dt)
         else:
             raise TypeError("The key is not DictTuple or Dict")
 
